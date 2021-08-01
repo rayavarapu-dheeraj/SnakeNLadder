5025b638c63b911eec5db82a0c3225abc0f4758d
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-01 09:03:26 +0530

    added: dice using random

--- src/com/bridgelabz/snakeandladder/Welcome.java
+++ src/com/bridgelabz/snakeandladder/Welcome.java
@@ -5,5 +5,8 @@ public class Welcome {
 		int start=0;
 		System.out.println("Welcome to Snake And Ladder game");
 		System.out.println("\nStarting the game with Game Point= " + start);
+		int dice = (int)(Math.random() * 6) + 1;
+		System.out.println("number on the dice is :" + dice);
+	
 	}
 }
