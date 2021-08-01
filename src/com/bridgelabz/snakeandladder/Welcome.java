f36fdaea7b125ff39caa1257b45b95c41660050f
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-01 09:00:20 +0530

    added: uc1 Start command

--- src/com/bridgelabz/snakeandladder/Welcome.java
+++ src/com/bridgelabz/snakeandladder/Welcome.java
@@ -2,6 +2,8 @@ package com.bridgelabz.snakeandladder;
 
 public class Welcome {
 	public static void main(String[] args) {
+		int start=0;
 		System.out.println("Welcome to Snake And Ladder game");
+		System.out.println("\nStarting the game with Game Point= " + start);
 	}
 }
