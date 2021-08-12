c5811c033c6ede5aef67e9d83486be7be08fb19d
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-12 08:32:31 +0530

    added : winners Position:

--- src/com/bridgelabz/snakeandladder/Welcome.java
+++ src/com/bridgelabz/snakeandladder/Welcome.java
@@ -1,22 +1,25 @@
 package com.bridgelabz.snakeandladder;
 
 public class Welcome {
-		final static int START = 0;
-		static final int NO_PLAY = 0;
-		static final int LADDER = 1;
-		static final int SNAKE = 2;
+	static final int START = 0;
+	static final int WIN_POSITION = 100;
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
 
-		public static void main(String[] args) {
-			System.out.println("Welcome to Snake And Ladder game");
-			System.out.println("\nStarting the game with Game Point= " + START);
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And Ladder game");
+		System.out.println("\nStarting the game with Game Point= " + START);
+
+		int position = 0;
+		int newPosition = 0;
+
+		while (position != WIN_POSITION) {
+
 			int dice = (int) (Math.random() * 6) + 1;
 			System.out.println("Dice roll: " + dice);
-			
-			int position = 0;
-			int newPosition = 0;
-			
 			int checkOption = (int) (Math.random() * 3);
-			
+
 			switch (checkOption) {
 			case NO_PLAY:
 				newPosition = 0;
@@ -31,6 +34,7 @@ public class Welcome {
 			if (position < 0) {
 				position = 0;
 			}
-			System.out.println("Position: "  + position);
+			System.out.println("Position: " + position);
 		}
+	}
 }
