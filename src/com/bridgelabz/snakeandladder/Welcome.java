a99bf9827cca166d5589e09b63be45ad964e41d0
rayavarapu-dheeraj <rayavarapu059@gmail.com>
2021-08-12 08:29:23 +0530

    added : Checks Options:

--- src/com/bridgelabz/snakeandladder/Welcome.java
+++ src/com/bridgelabz/snakeandladder/Welcome.java
@@ -1,12 +1,36 @@
 package com.bridgelabz.snakeandladder;
 
 public class Welcome {
-	public static void main(String[] args) {
-		int start=0;
-		System.out.println("Welcome to Snake And Ladder game");
-		System.out.println("\nStarting the game with Game Point= " + start);
-		int dice = (int)(Math.random() * 6) + 1;
-		System.out.println("number on the dice is :" + dice);
-	
-	}
+		final static int START = 0;
+		static final int NO_PLAY = 0;
+		static final int LADDER = 1;
+		static final int SNAKE = 2;
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Snake And Ladder game");
+			System.out.println("\nStarting the game with Game Point= " + START);
+			int dice = (int) (Math.random() * 6) + 1;
+			System.out.println("Dice roll: " + dice);
+			
+			int position = 0;
+			int newPosition = 0;
+			
+			int checkOption = (int) (Math.random() * 3);
+			
+			switch (checkOption) {
+			case NO_PLAY:
+				newPosition = 0;
+				break;
+			case LADDER:
+				newPosition = dice;
+				break;
+			case SNAKE:
+				newPosition = -dice;
+			}
+			position = position + newPosition;
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("Position: "  + position);
+		}
 }
